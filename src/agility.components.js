1ecc0319fdd86fff07188c83d7bb5891a02526aa
Hariz Hasecic <hariz.hasecic@gmail.com>
2021-05-23 15:18:11 +0200

    review stable

--- src/agility.components.js
+++ src/agility.components.js
@@ -1,20 +1,21 @@
 // Our Agility Modules
-import TextBlockWithImage from "./components/agility-pageModules/TextBlockWithImage";
-import RichTextArea from "./components/agility-pageModules/RichTextArea";
-import Heading from "./components/agility-pageModules/Heading";
-import FeaturedPost from "./components/agility-pageModules/FeaturedPost";
-import PostsListing from "./components/agility-pageModules/PostsListing";
-import PostDetails from "./components/agility-pageModules/PostDetails";
-import ContactForm from "./components/agility-pageModules/ContactForm";
-import BlueBanner from "./components/agility-pageModules/BlueBanner";
-import BigJumbotron from "./components/agility-pageModules/BigJumbotron";
-import ImageHeader from "./components/agility-pageModules/ImageHeader";
+import TextBlockWithImage from './components/agility-pageModules/TextBlockWithImage'
+import RichTextArea from './components/agility-pageModules/RichTextArea'
+import Heading from './components/agility-pageModules/Heading'
+import FeaturedPost from './components/agility-pageModules/FeaturedPost'
+import PostsListing from './components/agility-pageModules/PostsListing'
+import PostDetails from './components/agility-pageModules/PostDetails'
+import ContactForm from './components/agility-pageModules/ContactForm'
+import BlueBanner from './components/agility-pageModules/BlueBanner'
+import BigJumbotron from './components/agility-pageModules/BigJumbotron'
+import ImageHeader from './components/agility-pageModules/ImageHeader'
+import Reviews from './components/agility-pageModules/Reviews'
 
 // Our Agility Data Fetch
-import PostsListingData from "./data/PostsListing";
+import PostsListingData from './data/PostsListing'
 
 // Our Agility PageTemplates
-import MainTemplate from "./components/agility-pageTemplates/MainTemplate";
+import MainTemplate from './components/agility-pageTemplates/MainTemplate'
 
 export default {
 	dataFetch: {
@@ -31,8 +32,9 @@ export default {
 		BlueBanner,
 		BigJumbotron,
 		ImageHeader,
+		Reviews,
 	},
 	pageTemplateComponents: {
-		"Main Template": MainTemplate,
+		'Main Template': MainTemplate,
 	},
-};
+}
---
+++ src/components/agility-pageModules/Reviews.vue
@@ -0,0 +1,70 @@
+<template>
+	<div class="container mx-auto w-full sm:w-8/12 p-3">
+		<div v-for="(item, index) in item.fields.selectedReviews" :key="index" class="">
+			<div v-if="index == currentIndex" class="shadow-lg mx-3 xl:mx-3 sm:mx-0 lg:mx-0 rounded ">
+				<div class="p-5 relative ">
+					<p class="text-base text-gray-600 leading-8">{{ item.fields.review }}</p>
+					<div class="flex justify-end mt-2"></div>
+					<p class="text-right text-gray-600 p-3">{{ item.fields.customer }}</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		props: {
+			contentID: Number,
+			item: Object,
+			page: Object,
+			pageInSitemap: Object,
+			dynamicPageItem: Object,
+		},
+		filters: {
+			truncate(string, value) {
+				return string.substring(0, value) + '…'
+			},
+		},
+
+		data: function() {
+			return {
+				currentIndex: 0,
+			}
+		},
+
+		methods: {
+			getFigures() {},
+			moveForward() {
+				this.currentIndex = (this.currentIndex + 1) % this.item.fields.selectedReviews.length
+			},
+			movePrev() {},
+			startReviewFader() {
+				setInterval(this.moveForward, 4000)
+			},
+		},
+
+		computed: {
+			fields: function() {
+				if (this.item.fields.featuredPost) {
+					return this.item.fields.featuredPost.fields
+				} else {
+					return null
+				}
+			},
+			date: function() {
+				return new Date(this.fields?.date).toLocaleDateString()
+			},
+			category: function() {
+				if (this.fields.category) {
+					return this.fields.category.fields.title
+				} else {
+					return 'Uncategorized'
+				}
+			},
+		},
+		mounted() {
+			this.startReviewFader()
+		},
+	}
+</script>
